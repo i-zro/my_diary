aac925f6dd83220593a7d3977c3287fefd48d957
i-zro <dek3127@gmail.com>
2021-06-14 00:22:39 +0900

    2

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ host = "localhost"
 port = "27017"
 client=MongoClient(host, int(port))
 db=client['Users']
-
+mongo = MongoClient(host, int(port))
 det=[1,3]
 
 app=Flask(__name__)
@@ -144,7 +144,7 @@ def upload_file():
     #   if not ('../static/image_db/{0}'.format(det[1])):
     #       os.system(mkdir('../static/image_db/{0}'.format(det[1])))
       f.save('image_db/{0}_{1}'.format(det[1], datetime.now().strftime('%y년%m월%d일%H시%M분%S초'))+'.jpg')
-      insert_item_one(mongo, det[1], title, content, "diary", "diary_list", 'image_db/{0}_{1}'.format(det[1], datetime.now().strftime('%y년%m월%d일%H시%M분%S초'))+'.jpg')
+      insert_item_one(mongo, det[1], title, content, 'image_db/{0}_{1}'.format(det[1], datetime.now().strftime('%y년%m월%d일%H시%M분%S초'))+'.jpg', "diary", "diary_list")
       return render_template('return.html')
 
 @app.before_request
